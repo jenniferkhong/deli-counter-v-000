095afa90aa2686ae58fdc0e057207d3edb592669
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-17 21:20:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(array)
+  line_number = array.counter
+  line_list = []
+  x = 1
+
+  if line_number == 0
+    puts "The line is currently empty."
+  elsif line_number > 0
+    array.each do |person|
+      line_list << person.prepend("#{x}. ")
+      x += 1
+    end
+    puts "The line is currently: #{line_list.join(" ")}"
+  end
+end
+
+def take_a_number(array, name)
+  
